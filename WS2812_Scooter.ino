aeedc8757b2cebd510a91b037b7bc27c166b7a09
Wene <wene83@gmx.ch>
2017-12-12 22:03:52 +0100

    fixed global brightness

--- WS2812_Scooter.ino
+++ WS2812_Scooter.ino
@@ -5,14 +5,9 @@
 
 CRGBArray<NUM_LEDS> leds;
 
-
-int minBrightness = 10;
-int maxBrightness = 100;
-int brightness = minBrightness;
-
 void setup() {
     //setting maximum brightness
-    FastLED.setBrightness(brightness);
+    FastLED.setBrightness(80);
 
     FastLED.addLeds<WS2812, 10, GRB>(leds, NUM_LEDS); //GRB for the WS2812 color order
 
@@ -27,6 +22,7 @@ bool up = true;
 unsigned int loopCount = 0;
 int startPos = 0;
 uint8_t rainbowPos = 0;
+int brightness = 50;
 
 void loop()
 {
@@ -37,21 +33,21 @@ void loop()
     {
       if(iLED % 3 == 0)
       {
-        leds[iLED].setRGB(255, 0, 0);
+        leds[iLED].setRGB(brightness, 0, 0);
       }
       else if(iLED % 3 == 1)
       {
-        leds[iLED].setRGB(255, 255, 255);
+        leds[iLED].setRGB(brightness, brightness, brightness);
       }
       else
       {
-        leds[iLED].setRGB(0, 255, 0);
+        leds[iLED].setRGB(0, brightness, 0);
       }
     }
     if(up)
     {
       brightness++;
-      if(brightness >= maxBrightness)
+      if(brightness >= 200)
       {
         up = false;
       }
@@ -59,17 +55,15 @@ void loop()
     else
     {
       brightness--;
-      if(brightness <= minBrightness)
+      if(brightness <= 80)
       {
         up = true;
       }
     }
-    FastLED.setBrightness(brightness);
   }
   else if(loopCount < 4000)
   {
     leds.fadeToBlackBy(50);
-    FastLED.setBrightness(maxBrightness - 30);
     for(int iLED = startPos; iLED < startPos + 4; iLED++)
     {
       leds[iLED].r = 255;
@@ -83,6 +77,7 @@ void loop()
       startPos++;
       if(startPos >= NUM_LEDS - 4)
       {
+        startPos = NUM_LEDS - 4;
         up = false;
       }
     }
@@ -91,13 +86,13 @@ void loop()
       startPos--;
       if(startPos <= 0)
       {
+        startPos = 0;
         up = true;
       }
     }
   }
   else if(loopCount < 6000)
   {
-    FastLED.setBrightness(maxBrightness - 50);
     uint8_t hue = rainbowPos++;  // get start position and increase global position for next run
     for(int iLED = 0; iLED < NUM_LEDS; iLED++)
     {
